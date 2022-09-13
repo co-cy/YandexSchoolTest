4e84cc2205fd6fa6f48c49fc6f4b217066021483
_co_cy_ <stf2003.kem@gmail.com>
2022-09-13 19:51:30 +0300

    [develop] -> Changed the type of error during request validation

--- MyDisk/__init__.py
+++ MyDisk/__init__.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse, Response
+from fastapi.requests import Request
+from fastapi import FastAPI, status
 
 from MyDisk.routers import routes_initialization
 from MyDisk.config import FastApiConfig
@@ -11,3 +14,11 @@ node.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(**FastApiConfig())
 routes_initialization(app)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc) -> Response:
+    return JSONResponse(content={
+        "code": status.HTTP_400_BAD_REQUEST,
+        "message": "Невалидная схема документа или входные данные не верны."
+    }, status_code=status.HTTP_400_BAD_REQUEST)
