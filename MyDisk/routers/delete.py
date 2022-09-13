70808453d311c3524513281411994f254a355568
_co_cy_ <stf2003.kem@gmail.com>
2022-09-13 22:32:06 +0300

    [develop] -> Added validation for the /delete/{id} handler

--- MyDisk/routers/delete.py
+++ MyDisk/routers/delete.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi.exceptions import RequestValidationError
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import JSONResponse
+from datetime import datetime
 
 from MyDisk.database.models.node import Node
 from MyDisk.database import get_db
@@ -8,8 +11,24 @@ router = APIRouter()
 
 
 @router.delete("/delete/{node_id}")
-def delete(node_id: str, db_session=Depends(get_db)):
-    db_session.delete(db_session.query(Node).filter(Node.id == node_id).one())
+def delete(node_id: str, date: str, db_session=Depends(get_db)):
+
+    try:
+        datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%fZ")
+    except Exception:
+        raise RequestValidationError("Bad date")
+
+    node = db_session.query(Node).get(node_id)
+    if not node:
+        return JSONResponse(status_code=404, content={
+            "code": status.HTTP_404_NOT_FOUND,
+            "message": "Item not found"
+        })
+
+    db_session.delete(node)
     db_session.commit()
 
-    return "OK"
+    return JSONResponse(status_code=200, content={
+        "code": status.HTTP_200_OK,
+        "message": "Удаление прошло успешно."
+    })
